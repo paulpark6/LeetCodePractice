5f99110b73f1160d21c5cde4cfa49a6b30adeb40
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-11-18 15:53:49 -0700

    Update MergeTwoSortedLists.py

--- Paul/Easy/MergeTwoSortedLists.py
+++ Paul/Easy/MergeTwoSortedLists.py
@@ -1,17 +1,23 @@
-class ListNode:     
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        new_list = ListNode()
-        current = new_list
+        new = ListNode()
+        current = new
         while (list1 and list2):
-            if list1.val <= list2.val :
+            if (list1.val < list2.val):
                 current.next = list1
                 list1 = list1.next
-            elif list1.val > list2.val:
+            elif list2.val < list1.val:
+                current.next = list2
+                list2 = list2.next
+            elif list1.val == list2.val:
+                current.next = list1
+                list1 = list1.next
+                current = current.next
                 current.next = list2
                 list2 = list2.next
             current = current.next
@@ -19,4 +25,4 @@ class Solution:
             current.next = list1
         if list2:
             current.next = list2
-        return new_list.next
+        return new.next
