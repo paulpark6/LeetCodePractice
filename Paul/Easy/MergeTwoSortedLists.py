c020ad890ec69613351106267622c47d3091eab0
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-11-18 00:32:26 -0700

    Merge Two Sorted List
    need to revise how it points

--- Paul/Easy/MergeTwoSortedLists.py
+++ Paul/Easy/MergeTwoSortedLists.py
@@ -1,14 +1,22 @@
-def foo (list_a) -> int: 
-    item = list_a.pop(0)
-    print(item)
-
-k = list()
-k.append(11)
-k.append(2)
-k.append(234)
-print("before function")
-print(k)
-print("function call")
-foo(k)
-print("after function call")
-print(k)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = ListNode()
+        current = new_list
+        while (list1 and list2):
+            if list1.val <= list2.val :
+                current.next = list1
+                list1 = list1.next
+            elif list1.val > list2.val:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+        return new_list.next
