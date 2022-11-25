8f014d4c46391b490092c98e75c8c208a61e68ba
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-11-25 02:49:40 -0700

    search binary tree

--- Paul/Easy/InvertBinaryTree.py
+++ Paul/Easy/InvertBinaryTree.py
@@ -7,3 +7,14 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
+        if root is None:
+            return root
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        
+        return root
