fb46b23d2e57545af0a4be3b224dcc6b7e424e5f
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-12-02 21:21:38 -0700

    Update ValidParentheses.py

--- Paul/Easy/ValidParentheses.py
+++ Paul/Easy/ValidParentheses.py
@@ -1,15 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # dictionary value to have open as keys and close as values
-        pairs = {"{":"}", "(":")", "[":"]"}
-        # stack to store open brackets
         stack = list()
+        b = {"(":")", "[":"]", "{":"}"}
         for p in s:
-            # add to stack only if open
-            if p in pairs:
+            if p in b.keys():
                 stack.append(p)
-            # if its close check if the dict[stack[-1]] == closed bracket
-            elif stack and p in pairs.values() and pairs[stack[-1]] == p:
+            elif stack and p in b.values() and b[stack[-1]] == p:
                 stack.pop()
             else:
                 return False
