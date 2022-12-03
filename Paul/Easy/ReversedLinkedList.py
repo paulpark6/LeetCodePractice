035fed636104784d335ea043e392bbfd5a3c4322
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-12-03 02:11:47 -0700

    Update ReversedLinkedList.py

--- Paul/Easy/ReversedLinkedList.py
+++ Paul/Easy/ReversedLinkedList.py
@@ -5,16 +5,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # create stack add to stack by order
-        # create new list add by removing the top element in stack
-        stack = list()
-        new = ListNode()
-        ptr = head
-        while ptr:
-            stack.append(ptr.val)
-            ptr = ptr.next
-        ptr = new
-        while stack:
-            ptr.next = ListNode(stack.pop(), None)
-            ptr = ptr.next
-        return new.next
+        prev, cur = None, head
+        while (cur):
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
