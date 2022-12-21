8936d9a3077169e24a79a20c420aea452efb7975
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-12-20 22:07:30 -0700

    Update Palindrome.py

--- Paul/Easy/Palindrome.py
+++ Paul/Easy/Palindrome.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s = str(x)
+        s = str(x) 
         n = len(s)
-        for i in range(n//2):
+        for i in range(n//2): # loop to check for palindrome
             if s[i] != s[-(i+1)]:
                 return False
         return True
