b734ca60c7764d545f01a94fafec0492bc55d905
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-12-28 19:46:17 -0700

    Created BLACKBOX

--- Paul/Easy/SingleNumber.py
+++ Paul/Easy/SingleNumber.py
@@ -1,15 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # creat dic to store num and count
-        num_count = dict()
+        # dictionary with number as keys and count as values
+        counts = dict()
+        # loop through nums
         for num in nums:
-            if num not in num_count:
-                num_count[num] = 1
-            else: 
-                num_count[num] += 1
-        for num  in num_count:
-            if (num_count[num] == 1):
+            if num not in counts:
+                counts[num] = 1
+            else:
+                counts[num] += 1
+        for num in counts:
+            if counts[num] == 1:
                 return num
-
 
 # NEED TO USE ^ operator (Xor)
