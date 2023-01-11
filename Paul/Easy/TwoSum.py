93211861f0dbb8c9a170a793d3eb0ee189c72b45
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2023-01-10 22:43:47 -0700

    day1

--- Paul/Easy/TwoSum.py
+++ Paul/Easy/TwoSum.py
@@ -1,16 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # dictionary that stores numbers as keys and position as values
-        store = dict()
-        # loop 
+        # find = target - num
+        # use dictionary to store number and its position
+        # if find is found -> return number and its position form dictionary
+        d = dict()
         n = len(nums)
         for i in range(n):
             num = nums[i]
-            # find = target - num
             find = target - num
-            # if num not in dictionary add it
-            if num not in store:
-                store[num] = i
-            # if find in dictionary and different position return
-            if find in store and store[find] != i:
-                return [store[find], i]
+            if num not in d:
+                d[num] = i
+            if find in d and d[find] != i:
+                return [i,d[find]]
+                
